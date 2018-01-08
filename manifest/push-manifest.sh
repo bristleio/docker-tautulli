5c29e7dbf0f2db647f6cbd78175e7d864b79c8a6
Mike Davies <mike@mkdavies.com>
2018-01-08 14:13:31 -0800

    Set up manifest push

--- manifest/push-manifest.sh
+++ manifest/push-manifest.sh
@@ -7,5 +7,5 @@ curl -O -J -L https://github.com/estesp/manifest-tool/releases/download/v0.7.0/m
 chmod +x manifest-tool-linux-armv7
 
 # push the manifest
-./manifest-tool-linux-armv7 push from-spec manifest.yaml
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/manifest-tool-linux-armv7 push from-spec $DIR/manifest.yaml
