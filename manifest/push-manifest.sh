ce991cdca60001af10bf4807621441ecfa4b0d83
Mike Davies <mike@mkdavies.com>
2018-01-12 09:08:57 -0800

    Switching to amd64

--- manifest/push-manifest.sh
+++ manifest/push-manifest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # get the manifest tool
-curl -O -J -L https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-armv7
+curl -O -J -L https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
 
 # chmod it
-chmod +x manifest-tool-linux-armv7
+chmod +x manifest-tool-linux-amd64
 ls
 
 # push the manifest
-./manifest-tool-linux-armv7 push from-spec manifest.yaml
+manifest-tool-linux-amd64 push from-spec manifest.yaml
